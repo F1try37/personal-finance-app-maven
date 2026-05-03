51b13ba34b999b2718062a16316194a7a71c77a4
F1try37 <bildb20@yandex.ru>
2026-05-03 21:23:13 +0300

    add JacksonConfig

---
+++ src/main/java/com/example/financeapp/config/JacksonConfig.java
@@ -0,0 +1,13 @@
+package com.example.financeapp.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JacksonConfig {
+    private static final ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    public static ObjectMapper getMapper() {
+        return mapper;
+    }
+}
